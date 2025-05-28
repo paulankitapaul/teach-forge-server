0d167294e32fc011555e44829568bcae9aa60106
paulankitapaul <paulankita.ctg@gmail.com>
2025-05-28 14:03:01 +0600

    server initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.send('Teach Forge')
+})
+app.listen(port,()=>{
+    console.log(`server running on port: ${port}`);
+})
